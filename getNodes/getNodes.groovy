e4c221fe6a4f91463553799f70347050105df154
Sridhar Sarnobat <ss401533@gmail.com>
2013-06-15 12:31:02 -0700

    Minor

--- getNodes/getNodes.groovy
+++ getNodes/getNodes.groovy
@@ -1,4 +1,4 @@
-
+import org.apache.commons.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -9,13 +9,15 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 final String SERVER_ROOT_URI = "http://localhost:7474/db/data/";
-final String nodeEntryPointUri = SERVER_ROOT_URI + "node";
-// http://localhost:7474/db/data/node
- 
+final String nodeEntryPointUri = SERVER_ROOT_URI + "/index/auto/node/?query=name:*";
+// http://localhost:7474/db/data/db/data/index/auto/node/?query=name:*
+
 WebResource resource = Client.create()
-        .resource( SERVER_ROOT_URI );
-ClientResponse response = resource.get( ClientResponse.class );
- 
-System.out.println( String.format( "GET on [%s], status code [%d]",
-        SERVER_ROOT_URI, response.getStatus() ) );
+		.resource( SERVER_ROOT_URI );
+ClientResponse response = resource.accept(MediaType.APPLICATION_JSON).get( ClientResponse.class );
+
+String str = IOUtils.toString(response.getEntityInputStream(), "UTF-8");
+
+
+System.out.println( String.format( str ));
 response.close();
